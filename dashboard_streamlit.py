7d4c1c4b041a1a554a4767e0f33b4dc80523f2f6
putri <142087442+putri-indahsari@users.noreply.github.com>
2025-05-03 19:31:13 +0700

    Update dashboard_streamlit.py

--- dashboard_streamlit.py
+++ dashboard_streamlit.py
@@ -39,8 +39,13 @@ plt.xlabel('Musim')
 plt.ylabel('Rata-Rata Penyewaan')
 plt.legend(title='Tahun')
 st.pyplot(fig)
-
-# Tambahkan bagian ini di dashboard Streamlit Anda, bisa di bagian Pertanyaan Bisnis 1
+# insight/penjelasan
+st.markdown("""
+**Insight:**
+Rata-rata penyewaan sepeda lebih tinggi pada musim panas dibandingkan musim dingin. 
+Pertumbuhan penyewaan sepeda meningkat signifikan dari 2011 ke 2012, terutama pada musim panas. 
+Perbedaan antara musim panas dan dingin semakin lebar pada 2012, menunjukkan peluang strategi khusus musim dingin.
+""")
 
 st.subheader("Perbandingan Penyewaan Sepeda: Musim Panas vs Dingin (2011-2012)")
 
@@ -70,7 +75,6 @@ ax.set_xlabel('Tahun', fontsize=12)
 ax.set_ylabel('Rata-Rata Penyewaan Sepeda', fontsize=12)
 ax.legend(title='Musim', bbox_to_anchor=(1, 1))
 ax.grid(axis='y', linestyle='--', alpha=0.7)
-
 st.pyplot(fig)
 
 # Tambahkan insight/penjelasan
@@ -86,12 +90,26 @@ st.subheader("Rata-rata Suhu Udara")
 fig, ax = plt.subplots()
 sns.barplot(data=filtered, x='season_name', y='temp', palette='coolwarm', ax=ax)
 st.pyplot(fig)
+# insight/penjelasan
+st.markdown("""
+**Insight:**
+suhu rata-rata pada musim panas (Summer) jauh lebih tinggi dibandingkan musim dingin (Winter).
+Ini bisa menjadi salah satu alasan utama mengapa jumlah penyewaan sepeda meningkat saat musim panas—karena kondisi lebih nyaman untuk bersepeda. 
+Semakin tinggi suhu (dalam skala normalisasi), semakin nyaman untuk bersepeda. Jadi, musim panas → lebih banyak penyewaan.
+""")
 
 # Visualisasi kelembapan
 st.subheader("Rata-rata Kelembapan Udara")
 fig, ax = plt.subplots()
 sns.barplot(data=filtered, x='season_name', y='hum', palette='coolwarm', ax=ax)
 st.pyplot(fig)
+# insight/penjelasan
+st.markdown("""
+**Insight:**
+Kelembapan pada musim dingin selisish sedikit daripada musim panas.
+Kelembapan yang tinggi bisa membuat pengguna merasa tidak nyaman saat beraktivitas fisik seperti bersepeda karena Jika kelembapan tinggi,
+udara terasa lebih pengap dan tidak nyaman,ini bisa menyebabkan penurunan aktivitas luar ruangan seperti bersepeda.
+""")
 
 # Visualisasi kondisi cuaca
 filtered['weather_label'] = filtered['weathersit'].map({
@@ -105,18 +123,32 @@ fig, ax = plt.subplots(figsize=(10, 5))
 sns.barplot(data=filtered, x='weather_label', y='cnt', hue='season_name', ax=ax)
 plt.xticks(rotation=0)
 st.pyplot(fig)
+# insight/penjelasan
+st.markdown("""
+**Insight:**
+Musim panas menunjukkan penyewaan sepeda tertinggi, terutama saat cuaca cerah. Sebaliknya, cuaca buruk menurunkan penyewaan drastis, terutama di musim dingin. 
+Pola ini konsisten, menunjukkan preferensi pengguna untuk bersepeda lebih tinggi di musim panas terlepas dari kondisi cuaca.
+
+""")
 
 # Hari kerja vs libur
 st.subheader("Hari Kerja vs Libur: Summer vs Winter")
 fig, ax = plt.subplots()
 sns.barplot(data=filtered, x='workingday_label', y='cnt', hue='season_name', ax=ax)
 st.pyplot(fig)
+# insight/penjelasan
+st.markdown("""
+**Insight:**
+- Musim panas menunjukkan penyewaan tinggi baik saat hari kerja (~5700) maupun hari libur (~5500), menandakan tingginya aktivitas bersepeda untuk rekreasi dan aktivitas harian.
+Musim dingin justru lebih aktif saat hari kerja (~2800) dibanding hari libur (~2300), kemungkinan karena bersepeda digunakan sebagai moda transportasi rutin
+- promosi diasarankan fokus pada musim dingin pada pengguna rutin (pekerja/pelajar).
+Sediakan fasilitas pendukung seperti: Shelter untuk cuaca buruk,Jalur sepeda bebas salju,Sistem pelacakan real-time demi keamanan.
+""")
 
 # ===============================
 # PERTANYAAN BISNIS 2
 # ===============================
 st.header("Pertanyaan 2: Bagaimana pengaruh hari libur terhadap jumlah penyewaan sepeda pada tahun 2011–2012?")
-
 hour_df['year'] = hour_df['yr'].map({0: 2011, 1: 2012})
 
 # Hari libur per jam
@@ -126,6 +158,16 @@ hourly_avg = holiday_hourly.groupby(['hr', 'year'])['cnt'].mean().reset_index()
 fig, ax = plt.subplots()
 sns.lineplot(data=hourly_avg, x='hr', y='cnt', hue='year', palette={2011: 'skyblue', 2012: 'coral'}, ax=ax)
 st.pyplot(fig)
+# insight/penjelasan
+st.markdown("""
+**Insight:**
+- dari tahun 2011 ke 2012 mengalami peningkatan pola penyewaan,terlihat 2012 memiliki pola penyewaan tertinggi dibanding tahun sebelumnya 2011
+di kedua pola konsisten penyewaan tahun 2011 & 2012 memiliki kesamaan pola tren dijam yang hampir sama yakni dijam 11.00 siang - 14.00 dan juga di jam 16.00-18.00 sore
+mungkin di jam jam tersebut merupakan waktu yang ideal untuk aktivistas liburan bersepeda bersama terutaman setelah makan siang dan sebelum makan malam
+bisa saja penyewaan sepeda menjadi alternatif untuk wisata lokal /city tour karena cuaca di jam tersebut nyaman.
+untuk lebih meningkatkan penyewaan dibanding hari kerja terdapat rekomendasi seperti pembukaan jalur sepeda baru, peningkatan infrastruktur, 
+atau penambahan spot menarik yang dapat dikunjungi dengan sepeda.Lalu merupakan promosi dan kampanye hidup sehat / produktif untuk berolahraga bersepeda selama liburan
+""")
 
 # Hari libur per bulan
 st.subheader("Pola Penyewaan Sepeda per Bulan di Hari Libur")
@@ -137,6 +179,12 @@ sns.lineplot(data=monthly_avg, x='month', y='cnt', hue='year', marker='o', ax=ax
 ax.set_xticks(range(1, 13))
 ax.set_xticklabels(['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Agu', 'Sep', 'Okt', 'Nov', 'Des'])
 st.pyplot(fig)
+# insight/penjelasan
+st.markdown("""
+**Insight:**
+- Puncak penyewaan terjadi di Agustus, dengan lonjakan awal musim bersepeda pada Mei 2012. 
+Rekomendasi nya melakukan kolaborasi event lokal, prediksi berbasis cuaca, dan kampanye keluarga musiman. Fokus promosi di Mei sebagai pengganti bulan-bulan sepi (Desember–Februari) untuk optimalkan pendapatan.
+""")
 
 # Rata-rata penyewaan: libur vs kerja
 st.subheader("Rata-Rata Penyewaan: Hari Libur vs Hari Kerja")
@@ -145,6 +193,13 @@ holiday_avg = hour_df.groupby(['year', 'holiday_type'])['cnt'].mean().reset_inde
 fig, ax = plt.subplots()
 sns.barplot(data=holiday_avg, x='year', y='cnt', hue='holiday_type', palette={'Hari Libur': 'red', 'Hari Kerja': 'blue'}, ax=ax)
 st.pyplot(fig)
+# insight/penjelasan
+st.markdown("""
+**Insight:**
+- Penyewaan naik signifikan dari 2011 ke 2012, terutama di hari kerja (±62%).
+Untuk meningkatkan pendapatan rekomendasinya untuk tambah sepeda saat jam sibuk, 
+kerja sama penyediaan jalur commuting, dan tawarkan diskon langganan bagi pekerja/pelajar untuk mendorong penggunaan rutin.
+""")
 
 # Per jam: libur vs kerja
 st.subheader("Pola Penyewaan per Jam: Hari Libur vs Hari Kerja")
@@ -152,3 +207,10 @@ hourly_avg = hour_df.groupby(['hr', 'holiday_type'])['cnt'].mean().reset_index()
 fig, ax = plt.subplots()
 sns.lineplot(data=hourly_avg, x='hr', y='cnt', hue='holiday_type', palette={'Hari Kerja': 'blue', 'Hari Libur': 'red'}, ax=ax)
 st.pyplot(fig)
+# insight/penjelasan
+st.markdown("""
+**Insight:**
+- Hari kerja menunjukkan dua puncak penyewaan (7–9 & 16–19), 
+sementara hari libur lebih merata. Disarankan distribusi sepeda difokuskan ke area komuter saat jam sibuk, sistem harga dinamis, 
+serta paket langganan khusus komuter dan rekreasional untuk maksimalkan penggunaan di semua waktu.
+""")
